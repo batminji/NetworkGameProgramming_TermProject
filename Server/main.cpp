ca8f680586c7bd829b192cdcead3b111677973aa
D4LGONA(samsung) <laff3y2297@gmail.com>
2024-11-04 20:15:09 +0900

    241104 server, stdafx update

---
+++ Server/main.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+
+int client_thread(SOCKET s) // 클라이언트와의 통신 스레드
+{
+    std::cout << "접속 성공!" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    int retval;
+
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
+
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+    struct sockaddr_in serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVERPORT);
+    retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if (retval == SOCKET_ERROR) err_quit("bind()");
+
+    retval = listen(listen_sock, SOMAXCONN);
+    if (retval == SOCKET_ERROR) err_quit("listen()");
+
+    SOCKET client_sock;
+    struct sockaddr_in clientaddr;
+    int addrlen;
+
+    std::vector<std::thread> threads;  // std::thread를 저장할 벡터
+
+    while (true) {
+        addrlen = sizeof(clientaddr);
+        client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+        if (client_sock == INVALID_SOCKET) {
+            std::cerr << "accept() failed" << std::endl;
+            break;
+        }
+
+        // 스레드 생성...
+        threads.emplace_back(client_thread, client_sock);
+    }
+
+    for (auto& th : threads) {
+         th.join();
+    }
+
+    closesocket(listen_sock);
+    WSACleanup();
+}
--- Server/stdafx.h
+++ Server/stdafx.h
@@ -1,19 +1,26 @@
 #pragma once
 
 #define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
 #include <fstream>
+#include <winsock2.h> // 절대 순서를 바꾸지 말 것..
 #include <windows.h>
+#include <ws2tcpip.h>
 #include <tchar.h>
-#include <string.h>
 #include <algorithm>
 #include <random>
+#include <thread>
 #include <time.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <vector>
+#include <string>
+#include <stdio.h> 
+#include <stdlib.h>
 #include <atlImage.h>
 #include "..\Kirby\resource.h"
+#pragma comment(lib, "ws2_32") // ws2_32.lib 링크
 
 // enum
 enum PACKET // 패킷 타입 정의.
@@ -45,4 +52,45 @@ enum OTYPE // 오브젝트 타입 정의.
 // 상수
 constexpr int ID_LEN = 20;
 constexpr int MAX_OBJ_NUM = 100; // todo: 여기 상의
+constexpr int SERVERPORT = 9000;
+constexpr int BUFSIZE = 512; // todo: 여기 상의
 
+// 소켓 오류 관련
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 소켓 함수 오류 출력
+void err_display(int errcode)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, errcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[오류] %s\n", (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
