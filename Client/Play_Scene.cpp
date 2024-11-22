e8a49bd3d3738cb46faa92af8d879a96431597f4
코딩천재이종만 <jongman@tukorea.ac.kr>
2024-11-22 13:58:44 +0900

    recv_player_move()만듦
    될지는 모름

--- Client/Play_Scene.cpp
+++ Client/Play_Scene.cpp
@@ -90,7 +90,7 @@ void Play_Scene::update()
     //플레이어인풋 전송
     send_player_input(send_y);
     //플레이어 데이터 수신
-    //recv_player_data();
+    recv_player_data();
     
 }
 
@@ -150,6 +150,30 @@ int Play_Scene::send_player_input(short y)
     }
 
     
+    return 1;
+}
+
+int Play_Scene::recv_player_data()
+{
+    //플레이어 좌표 받기
+    char recvBuf[BUFSIZE];
+    int recvLen = recv(*m_sock, recvBuf, sizeof(SC_PLAYER_MOVE_PACKET), 0);
+    if (recvLen <= 0) {
+        std::cerr << "플레이어 좌표받기 실패" << std::endl;
+        return -1;
+    }
+    else {
+        SC_PLAYER_MOVE_PACKET* resPacket = reinterpret_cast<SC_PLAYER_MOVE_PACKET*>(recvBuf);
+        if (master_player->who_is_me) {
+            master_player->y = resPacket->this_y;
+            join_player->y = resPacket->other_y;
+        }
+        else {
+            master_player->y = resPacket->other_y;
+            join_player->y = resPacket->this_y;
+        }
+    }
+
     return 1;
 }
 
