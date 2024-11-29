b941f01af923e456c3aaf99f8de14cbfe0d7d424
BATMINJI <105769332+batminji@users.noreply.github.com>
2024-11-29 13:29:24 +0900

    Revert "play scene sound 추가"
    This reverts commit 043675c15c6c31650cdefe0558fddd2b55e06209.

--- Client/Play_Scene.h
+++ Client/Play_Scene.h
@@ -25,17 +25,17 @@ private:
     CImage* number;
     CImage* heart;
 public:
-    System* ssystem = nullptr;
-    Sound* battle_bgm = nullptr;
-    Sound* damage_sound = nullptr;
-    Sound* gameover_sound = nullptr;
-    Sound* skill_sound = nullptr;
-    Sound* explosion_sound = nullptr;
-    Sound* warning_sound = nullptr;
-    Sound* coin_sound= nullptr;
+    /*System* ssystem;
+    Sound* battle_bgm;
+    Sound* damage_sound;
+    Sound* gameover_sound;
+    Sound* skill_sound;
+    Sound* explosion_sound;
+    Sound* warning_sound;
+    Sound* coin_sound;
     Channel* channel = 0;
     FMOD_RESULT result;
-    void* extradriverdata = 0;
+    void* extradriverdata = 0;*/
 private :
     //play inform
     int bg_xPos = 800; // 배경 사진 xPos
